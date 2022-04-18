b1c6509251cb84280409cc6e47ee40423376fb51
dobinoo <dobinoo5@gmail.com>
2022-04-18 15:49:44 +0100

    Dictionary test testing for better searching the key words

--- Dictionary_test.py
+++ Dictionary_test.py
@@ -1,20 +1,21 @@
-# word_list ={"futbal" : ["brankar", "gula", "lopta", "futbalista", "hrac", "rozhodca", "trener", "dvaja treneri", "dvojity utok"],
-# "spanok": ["postel", "pyzamo", "budik", "spanok"]}
+word_list ={"futbal" : ["brankar", "gula", "lopta", "futbalista", "hrac", "rozhodca", "trener", "dvaja treneri", "dvojity utok"],
+ "spanok": ["postel", "pyzamo", "budik", "spanok"]}
 
-#user_input = input()
+user_input = input()
 #print(word_list["futbal"])
 #print(len(word_list))
 #search_value = input()
 
-# for key, value in word_list.items():
-#     for word in value:
-#         if word == search_value:
-#             print(key)
+for key, value in word_list.items():
+    for word in value:
+        if word in user_input:
+            print(key)
 
-test_string = "Toto je testovaci string a idem vyskusat ci dokazem spravne vyhladavat stringy"
-hladany_string = input()
 
-if hladany_string in test_string:
-    print("found")
-else:
-    print("not_found")
+# test_string = "Toto je testovaci string a idem vyskusat ci dokazem spravne vyhladavat stringy"
+# hladany_string = input()
+#
+# if hladany_string in test_string:
+#     print("found")
+# else:
+#     print("not_found")
